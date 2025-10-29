16734c7cf4acdde05bc23b655b8d7ea69b288eaa
Thiago Cadamuro <ibst.thiago@gmail.com>
2025-10-29 18:31:59 -0300

    feat: add Material UI dependencies to frontend

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,28 +1,59 @@
-import estacioLogo from '../assets/logos/estacio_white_logo.svg';
-import phoneIcon from '../assets/icons/phone_icon.svg';
-import whatsappIcon from '../assets/icons/whatsapp_icon.svg';
+import { Box, Typography } from "@mui/material";
+import estacioLogo from "../assets/logos/estacio_white_logo.svg";
+import phoneIcon from "../assets/icons/phone_icon.svg";
+import whatsappIcon from "../assets/icons/whatsapp_icon.svg";
 
 export default function Footer() {
     return (
-        <footer className="bg-primary-action-low text-white py-6">
-            <div className="max-w-[1366px]  mx-auto">
-                <div className="flex items-center justify-between">
-                    <img 
-                        src={estacioLogo} 
-                        alt="Logo Estácio" 
-                    />
-                    <div className="flex gap-14">
-                        <div className="flex items-center gap-3">
-                            <img src={phoneIcon} alt="Telefone"/>
-                            <span className="font-medium">0800 771 5055</span>
-                        </div>
-                        <div className="flex items-center gap-3">
-                            <img src={whatsappIcon} alt="WhatsApp" />
-                            <span className="font-medium">Precisa de ajuda?</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <Box
+            component="footer"
+            className="bg-primary-action-low text-white py-6"
+        >
+            <Box className="max-w-[1366px] mx-auto">
+                <Box className="flex items-center justify-between">
+                    <Box component="img" src={estacioLogo} alt="Logo Estácio" />
+                    <Box className="flex gap-14">
+                        <Box className="flex items-center gap-3">
+                            <Box
+                                component="img"
+                                src={phoneIcon}
+                                alt="Telefone"
+                            />
+                            <Typography
+                                sx={{
+                                    fontWeight: "600",
+                                    fontSize: "16px",
+                                    leadingTrim: "NONE",
+                                    lineHeight: "150%",
+                                    letterSpacing: "0%",
+                                    color: "white",
+                                }}
+                            >
+                                0800 771 5055
+                            </Typography>
+                        </Box>
+                        <Box className="flex items-center gap-3">
+                            <Box
+                                component="img"
+                                src={whatsappIcon}
+                                alt="WhatsApp"
+                            />
+                            <Typography
+                                sx={{
+                                    fontWeight: "600",
+                                    fontSize: "16px",
+                                    leadingTrim: "NONE",
+                                    lineHeight: "150%",
+                                    letterSpacing: "0%",
+                                    color: "white",
+                                }}
+                            >
+                                Precisa de ajuda?
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,14 @@
+import { Box } from '@mui/material';
 import estacioLogo from '../assets/logos/estacio_black_logo.svg';
 
 export default function Header() {
     return (
-        <header>
-            <div className="max-w-[1366px] mx-auto py-6">
-                <div className="flex items-center">
-                    <img 
-                        src={estacioLogo} 
-                        alt="Logo Estácio" 
-                    />
-                </div>
-            </div>
-        </header>
+        <Box component="header" className="py-6 max-w-[1366px] mx-auto">
+            <Box
+                component="img"
+                src={estacioLogo} 
+                alt="Logo Estácio"
+            />
+        </Box>
     );
 }
